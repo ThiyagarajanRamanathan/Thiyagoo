0c40e9c2a3056439944c999eebc9793ce3058494
ThiyagarajanRamanathan <posttothiyagarajan@gmail.com>
2017-10-08 19:27:16 +0530

    Added program exit message

--- MyFirstProject.java
+++ MyFirstProject.java
@@ -1,10 +1,9 @@
-
-
 class MyFirstProject
 {
    public static void main(String[] args)
    {
        System.out.println("My First Java git hup file...");
+       System.out.println("Exit...");
    }
 
 }
